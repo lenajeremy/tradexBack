216d013883e7d82103b765b51a0e5eacaac1dd4c
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-10-21 16:26:40 -0700

    added s3 bucket configurations

--- tradex/settings.py
+++ tradex/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'buyAndSell',
     'userAuthentication',
-    'corsheaders'
+    'corsheaders',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -136,3 +137,25 @@ STATICFILES_DIRS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
+
+
+#amazon s3 configurations
+
+AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID ='AKIAILAZ34TWJCG63CCA' 
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME ='tradexmediafiles'
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {    
+     'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'app.storage_backends.MediaStorage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] 
+STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+AWS_DEFAULT_ACL = None
