f2d72376b731daf62fe2082a5e45a851e287ac3e
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-10-30 00:19:06 -1100

    jkflasd

--- tradex/settings.py
+++ tradex/settings.py
@@ -151,13 +151,13 @@ AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_S3_OBJECT_PARAMETERS = {    
      'CacheControl': 'max-age=86400',
 }
-# DEFAULT_FILE_STORAGE = 'tradex.storage_backend.MediaStorage'
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# STATICFILES_DIRS = [
+DEFAULT_FILE_STORAGE = 'tradex.storage_backend.MediaStorage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ] 
-# STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-# STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
-# AWS_DEFAULT_ACL = None
+AWS_DEFAULT_ACL = None
