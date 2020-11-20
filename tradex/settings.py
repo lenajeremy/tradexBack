940208779698586601b07af389edbe2124fde1e5
Jeremiah Lena <jeremiahlena13@gmail.com>
2020-11-20 17:48:31 +0100

    commit

--- tradex/settings.py
+++ tradex/settings.py
@@ -144,22 +144,22 @@ MEDIA_URL = '/media/'
 
 #AMAZON S3 CONFIGURATIONS
 
-# AWS_LOCATION = 'static'
-# AWS_ACCESS_KEY_ID =config('AWS_ACCESS_KEY_ID')
-# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-# AWS_STORAGE_BUCKET_NAME ='tradexmediafiles'
-# AWS_S3_ADDRESSING_STYLE = 'virtual'
-# AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# AWS_S3_OBJECT_PARAMETERS = {    
-#      'CacheControl': 'max-age=86400',
-# }
-# DEFAULT_FILE_STORAGE = 'tradex.storage_backend.MediaStorage'
-# STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ] 
-# STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-# ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
-# STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-# )
-# AWS_DEFAULT_ACL = None
+AWS_LOCATION = 'static'
+AWS_ACCESS_KEY_ID =config('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME ='tradexmediafiles'
+AWS_S3_ADDRESSING_STYLE = 'virtual'
+AWS_S3_CUSTOM_DOMAIN='%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {    
+     'CacheControl': 'max-age=86400',
+}
+DEFAULT_FILE_STORAGE = 'tradex.storage_backend.MediaStorage'
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+] 
+STATIC_URL='https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
+STATICFILES_FINDERS = (           'django.contrib.staticfiles.finders.FileSystemFinder',    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+)
+AWS_DEFAULT_ACL = None
