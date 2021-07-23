569ad9e8584c19b188be907cdd27df3ecdb5d38c
Jeremiah Lena <jeremiahlena13@gmail.com>
2021-07-23 13:28:47 -0700

    changed models

--- userAuthentication/models.py
+++ userAuthentication/models.py
@@ -9,10 +9,9 @@ class User(AbstractUser):
     ('seller', 'Seller')
   )
   userType = models.CharField(choices=USERTYPE_CHOICES, default='buyer', max_length=9)
-  profile_picture = models.ImageField(blank = True, upload_to = 'profile_images', default = 'profile_images/avatar.jpg')
-  cover_picture = models.ImageField(blank = True, upload_to = 'cover_pictures', default = 'cover_pictures/cover.jpeg')
+  profile_picture = models.TextField(blank = True)
+  cover_picture = models.TextField(blank = True)
   paypal_email_address = models.EmailField()
-  # ref_key = models.TextField(unique = True)
   
   def getProducts(self):
     if self.userType == 'buyer':
@@ -22,13 +21,13 @@ class User(AbstractUser):
   def serialize(self, isSelf):
     data_to_return = None
     if isSelf:
-      data_to_return = {'id': self.id, 'userName': self.username, 'firstName': self.first_name, 'lastName': self.last_name, 'profilePicture': self.profile_picture.url, 'postsMade': [post.serialize(self) for post in self.posts.order_by('-dateCreated')], 'userType': self.userType, 'accountDetails': self.account.serialize(), 'emailAddress': self.email, 'paypalEmail': self.paypal_email_address, 'profile': self.profile.serialize(), 'coverPicture': self.cover_picture.url, 'latestMessages': [conversation.messages.last().serialize() for conversation in self.conversation.order_by('-last_modified')]}
+      data_to_return = {'id': self.id, 'userName': self.username, 'firstName': self.first_name, 'lastName': self.last_name, 'profilePicture': self.profile_picture, 'postsMade': [post.serialize(self) for post in self.posts.order_by('-dateCreated')], 'userType': self.userType, 'accountDetails': self.account.serialize(), 'emailAddress': self.email, 'paypalEmail': self.paypal_email_address, 'profile': self.profile.serialize(), 'coverPicture': self.cover_picture, 'latestMessages': [conversation.messages.last().serialize() for conversation in self.conversation.order_by('-last_modified')]}
       if self.userType == 'buyer':
         data_to_return['cart'] = self.cart.serialize()
       else:
         data_to_return['products'] = self.store.serialize()
     else:
-      data_to_return = {'id': self.id, 'userName': self.username,'firstName': self.first_name, 'lastName': self.last_name, 'profilePicture': self.profile_picture.url, 'postsMade': [post.serialize(self) for post in self.posts.order_by('-dateCreated')], 'userType': self.userType, 'profile': self.profile.serialize(), 'coverPicture': self.cover_picture.url}
+      data_to_return = {'id': self.id, 'userName': self.username,'firstName': self.first_name, 'lastName': self.last_name, 'profilePicture': self.profile_picture, 'postsMade': [post.serialize(self) for post in self.posts.order_by('-dateCreated')], 'userType': self.userType, 'profile': self.profile.serialize(), 'coverPicture': self.cover_picture}
       
     return data_to_return
   
@@ -38,7 +37,7 @@ class User(AbstractUser):
 
 class User_profile(models.Model):
   user = models.OneToOneField(User, on_delete = models.CASCADE, related_name = 'profile')
-  bio = models.CharField(max_length = 200, default='About Me')
+  bio = models.CharField(max_length = 200, default='A short introduction about yourself')
   status = models.CharField(max_length = 60, default = 'Currently Available')
   
   def __str__(self):
